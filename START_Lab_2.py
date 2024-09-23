a947c5774b025da90214c1706810d60478c57569
Alex Yang <ysg13579@gmail.com>
2024-09-23 04:03:10 -0600

    question 5

--- START_Lab_2.py
+++ START_Lab_2.py
@@ -1,25 +1,40 @@
-def lab2Question1(word):
+def lab2Question1(word:str) -> bool:
     # Note - you'll need to change the signature (above) to match the arguments for this lab...
     # Create a function that takes in a string 
     # Return True if that string is a palindrome, False otherwise
-    pass
+    word = word.replace(" ","")
+    return word == word[::-1]
 
-def lab2Question2(number_val):
+
+
+def lab2Question2(number_val: int) -> list:
     # Create a function that takes in a number
     # Return a list of the fibonacci sequence up to that number
-    pass
+    fib_seq = [0,1]
 
-def lab2Question3(str1, str2):
+    while True:
+        next_val = fib_seq[-1] + fib_seq[-2]
+        if next_val > number_val:
+            break
+        fib_seq.append(next_val)
+
+    return fib_seq if number_val >= 0 else []
+
+def lab2Question3(str1: str, str2: str) -> int:
     # Create a function that takes in two strings - str1 and str2
     # Return the number of times str2 appears in str1
     # For example if str1 = "coding is cool" and str2 = "co" then output should be 2.
-    pass
+    return str1.count(str2)
 
 def lab2Question4(list1, list2):
     # Create a function that takes in two equal length list of numbers. 
     # Return a list of the element-wise sum of the two lists - i.e. the first element of the output list is the sum of the first elements of the input lists
     # If the condition of the function is not satisfied, return a blank list
-    pass
+    if len(list1) != len(list2):
+        return []
+    sum_list = []
+    for i in range(len(list1)):
+        sum_list.append(list1[i] + list2[i])
 
     return sum_list
 
@@ -35,14 +50,42 @@ def lab2Question5():
     # that takes in a password and returns True if the password is valid, False otherwise.
     # You will need to make that function, exactly as described above. 
     password = None
+    while True:
+        password = input("Enter a password: ")
+        if isValidPassword(password):
+            print("Password is valid.")
+            return password
+        else:
+            print("Invalid password. Please try again.")
+
 
     return password
 
-def isValidPassword(password):
+def isValidPassword(password: str) -> bool:
     # Create a function that takes in a password and returns True if the password is valid, False otherwise
     # - At least 8 characters long
     # - Contains at least one uppercase letter
     # - Contains at least one lowercase letter
     # - Contains at least one number
-    pass
+    
+    if len(password) < 8:
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if not any(char.islower() for char in password):
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    return True
 
+## test
+def test_lab2Question1():
+    assert lab2Question1("purple") == False, "Test Case 1 Failed"
+    print("test passed")
+test_lab2Question1()
+
+print(lab2Question2(10))
+print(lab2Question3("coding is coold", "o"))
+print(lab2Question4([1,2,3],[4,5,6]))
+valid_password = lab2Question5()
+print("Valid password", valid_password)
